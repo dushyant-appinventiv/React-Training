210094a33b7fa2a24d5e88b5402e952911127bb3
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-06-02 09:55:26 +0530

    added home component

---
+++ src/modules/home/index.js
@@ -0,0 +1,23 @@
+import React , {Component} from "react";
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{"Hello form Home"}</h1>
+            </div>
+        );
+    }
+}
+
+export class Home1 extends Component{
+    render(){
+        return(
+            <div>
+                <h1> {"Hello from Home1"} </h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
